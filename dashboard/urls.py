e55f7c7d8eb8fc3f3f115fafbce5227786c5ccb0
rahul292-cloud <imrsy292@gmail.com>
2021-02-12 18:55:53 +0530

    [chg in templates]

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 # from .views import ForgotPasswordView
 from .views import Dashboard
 urlpatterns = [
-    path('storebuddy/', Dashboard.as_view(), name='storebuddy'),
+    # path('storebuddy/', Dashboard.as_view(), name='storebuddy'),
+    path('', Dashboard.as_view(), name='storebuddy'),
 ]
